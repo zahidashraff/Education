80ffb1fe7f27fec710b129d0a12a19f7f9d60f8e
Rydber1102 <rydber1102@gmail.com>
2025-10-09 16:51:45 +0800

    new update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import menu_icon from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
@@ -13,17 +14,29 @@ const Navbar = () => {
         })
     },[]);
 
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const toggleMenu = ()=>{
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
+
   return (
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
         <img src={logo} alt="" className='logo' />
-        <ul>
-            <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-            <li>Program</li>
-            <li>About us</li>
-            <li>Campus</li>
-            <li>Feedback</li>
-            <li><button className='btn'>Contact us</button></li>
+        <ul className={mobileMenu?'':'hide-mobile-menu'}>
+            <li><Link to='hero' smooth={true} offset={0} duration={500}
+            >Home</Link></li>
+            <li><Link to='program' smooth={true} offset={-260} duration={500}
+            >Program</Link></li>
+            <li><Link to='about' smooth={true} offset={-150} duration={500}
+            >About us</Link></li>
+            <li><Link to='campus' smooth={true} offset={-260} duration={500}
+            >Campus</Link></li>
+            <li><Link to='testimonials' smooth={true} offset={-260} duration={500}
+            >Feedback</Link></li>
+            <li><Link to='contact' smooth={true} offset={-260} duration={500}
+            className='btn'>Contact us</Link></li>
         </ul>
+        <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
 }
